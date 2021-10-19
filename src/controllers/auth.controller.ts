a9428d98f6a8e3d053796e2c5d9c247b07c9302a
aryan249 <aryan.soni249@gmail.com>
2021-10-19 14:13:59 +0530

    Add login controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -22,3 +22,28 @@ export const register = async (req: Request<{}, {}, RegisterBody>, res: Response
 
   res.status(201).json({ user: result.rows[0] });
 };
+
+export const login = async (req: Request<{}, {}, LoginBody>, res: Response) => {
+  const { email, password } = req.body;
+
+  const result = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+  if (result.rows.length === 0) {
+    res.status(401).json({ error: 'Invalid email or password' });
+    return;
+  }
+
+  const user = result.rows[0];
+  const validPassword = await bcrypt.compare(password, user.password);
+  if (!validPassword) {
+    res.status(401).json({ error: 'Invalid email or password' });
+    return;
+  }
+
+  const token = jwt.sign(
+    { id: user.id, email: user.email },
+    config.jwtSecret,
+    { expiresIn: config.jwtExpiresIn as any }
+  );
+
+  res.json({ token, user: { id: user.id, email: user.email } });
+};
