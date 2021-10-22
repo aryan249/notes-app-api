ba766e20faa4750876d7f46a6484d651153b38c8
aryan249 <aryan.soni249@gmail.com>
2021-10-22 21:04:40 +0530

    Add routes barrel exports

---
+++ src/routes/index.ts
@@ -0,0 +1,5 @@
+export { default as authRoutes } from './auth.routes';
+export { default as notesRoutes } from './notes.routes';
+export { default as userRoutes } from './user.routes';
+export { default as tagsRoutes } from './tags.routes';
+export { default as statsRoutes } from './stats.routes';
