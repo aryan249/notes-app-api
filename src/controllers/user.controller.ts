1ad1160d6c734b9147861769f9dd76d022219619
aryan249 <aryan.soni249@gmail.com>
2021-11-02 23:36:24 +0530

    Add changePassword controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -19,3 +19,25 @@ export const getProfile = async (req: AuthRequest, res: Response) => {
 
   res.json({ user: result.rows[0] });
 };
+
+export const changePassword = async (req: AuthRequest, res: Response) => {
+  const userId = req.user!.id;
+  const { currentPassword, newPassword } = req.body;
+
+  const result = await pool.query('SELECT password FROM users WHERE id = $1', [userId]);
+  if (result.rows.length === 0) {
+    res.status(404).json({ error: 'User not found' });
+    return;
+  }
+
+  const valid = await bcrypt.compare(currentPassword, result.rows[0].password);
+  if (!valid) {
+    res.status(401).json({ error: 'Current password is incorrect' });
+    return;
+  }
+
+  const hashedPassword = await bcrypt.hash(newPassword, config.bcryptRounds);
+  await pool.query('UPDATE users SET password = $1 WHERE id = $2', [hashedPassword, userId]);
+
+  res.json({ message: 'Password updated successfully' });
+};
