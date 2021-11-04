fabefb59038bd7e656ba59d5fb3204602e797371
aryan249 <aryan.soni249@gmail.com>
2021-11-04 17:12:53 +0530

    Add error handling and graceful shutdown

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import notesRoutes from './routes/notes.routes';
 import userRoutes from './routes/user.routes';
 import tagsRoutes from './routes/tags.routes';
 import statsRoutes from './routes/stats.routes';
+import { errorHandler, notFoundHandler } from './middleware/error.middleware';
 import { apiLimiter } from './middleware/rateLimit.middleware';
 import { requestId } from './middleware/requestId.middleware';
 import pool from './db/pool';
@@ -37,5 +38,23 @@ app.use('/api/notes', notesRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/tags', tagsRoutes);
 app.use('/api/stats', statsRoutes);
+
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+const server = app.listen(config.port, () => {
+  console.log(`Server running on port ${config.port}`);
+});
+
+const shutdown = (signal: string) => {
+  console.log(`\n${signal} received. Shutting down gracefully...`);
+  server.close(() => {
+    console.log('Server closed');
+    process.exit(0);
+  });
+};
+
+process.on('SIGTERM', () => shutdown('SIGTERM'));
+process.on('SIGINT', () => shutdown('SIGINT'));
 
 export default app;
