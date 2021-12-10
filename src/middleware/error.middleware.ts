190d019036472d03d394144db314faa522b80e29
aryan249 <aryan.soni249@gmail.com>
2021-12-10 10:40:38 +0530

    Add errorHandler middleware

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -9,3 +9,18 @@ export class AppError extends Error {
     this.name = 'AppError';
   }
 }
+
+export const errorHandler = (
+  err: Error,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) => {
+  if (err instanceof AppError) {
+    res.status(err.statusCode).json({ error: err.message });
+    return;
+  }
+
+  console.error('Unhandled error:', err);
+  res.status(500).json({ error: 'Internal server error' });
+};
