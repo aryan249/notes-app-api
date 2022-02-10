e4b52e09d8194ff2ee7aa26912647067ce6c1d9c
aryan249 <aryan.soni249@gmail.com>
2022-02-10 22:45:25 +0530

    Add health check endpoint

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import { config } from './config';
 import { requestId } from './middleware/requestId.middleware';
+import pool from './db/pool';
 
 const app = express();
 
@@ -14,5 +15,14 @@ if (config.nodeEnv !== 'test') {
   app.use(morgan('dev'));
 }
 app.use(express.json({ limit: '10kb' }));
+
+app.get('/health', async (_req, res) => {
+  try {
+    await pool.query('SELECT 1');
+    res.json({ status: 'ok', database: 'connected' });
+  } catch {
+    res.status(503).json({ status: 'error', database: 'disconnected' });
+  }
+});
 
 export default app;
