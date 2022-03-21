66869061cad5daec10eaed2f042059977a09af5f
aryan249 <aryan.soni249@gmail.com>
2022-03-21 10:23:45 +0530

    Add Note interface

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,3 +4,15 @@ export interface User {
   password: string;
   created_at: Date;
 }
+
+export interface Note {
+  id: number;
+  title: string;
+  content: string;
+  tags: string[];
+  archived: boolean;
+  pinned: boolean;
+  created_at: Date;
+  updated_at: Date;
+  user_id: number;
+}
