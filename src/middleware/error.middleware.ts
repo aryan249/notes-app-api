e98bc746a28f536bf54a881842e5ea068cdaf05c
aryan249 <aryan.soni249@gmail.com>
2022-04-04 19:56:32 +0530

    Add AppError class

---
+++ src/middleware/error.middleware.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from 'express';
+
+export class AppError extends Error {
+  constructor(
+    public statusCode: number,
+    message: string
+  ) {
+    super(message);
+    this.name = 'AppError';
+  }
+}
