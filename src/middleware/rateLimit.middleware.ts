0a97fdb41417127dacf157de33dc212b9ec6f2c9
aryan249 <aryan.soni249@gmail.com>
2022-04-05 09:27:49 +0530

    Add auth rate limiter

--- src/middleware/rateLimit.middleware.ts
+++ src/middleware/rateLimit.middleware.ts
@@ -7,3 +7,11 @@ export const apiLimiter = rateLimit({
   legacyHeaders: false,
   message: { error: 'Too many requests, please try again later' },
 });
+
+export const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 20,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: { error: 'Too many auth attempts, please try again later' },
+});
